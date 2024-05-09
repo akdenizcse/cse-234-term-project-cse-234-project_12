c8d2b08661691ad42b5a9e289443b1055c97c317
İsmail Temüroğlu <ismailtemuroglu2819@gmail.com>
2024-05-09 22:38:45 +0300

    finish top app bar

---
+++ app/src/main/java/com/example/health_tracker/HealthTrackerScreen.kt
@@ -0,0 +1,50 @@
+package com.example.health_tracker
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+enum class HealthTrackerScreen(@StringRes val title: Int) {
+    Login(title = R.string.login),
+    SignUp(title = R.string.sign_up),
+    Profile(title = R.string.profile),
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HealthTrackerAppBar(
+    currentScreen: HealthTrackerScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    TopAppBar(
+        title = { Text(text = stringResource(id = currentScreen.title))},
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+
+
+    )
+}
+
+@Composable
+fun HealthTrackerApp() {
+
+}
