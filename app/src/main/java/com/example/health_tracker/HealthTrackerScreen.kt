5a144b55130de1b730d48311cace59a1eeed9ea0
İsmail Temüroğlu <ismailtemuroglu2819@gmail.com>
2024-05-09 22:56:46 +0300

    add navhost and some string values

--- app/src/main/java/com/example/health_tracker/HealthTrackerScreen.kt
+++ app/src/main/java/com/example/health_tracker/HealthTrackerScreen.kt
@@ -1,16 +1,23 @@
 package com.example.health_tracker
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 enum class HealthTrackerScreen(@StringRes val title: Int) {
     Login(title = R.string.login),
@@ -44,7 +51,37 @@ fun HealthTrackerAppBar(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HealthTrackerApp() {
+    val navController = rememberNavController()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = HealthTrackerScreen.valueOf(
+        backStackEntry?.destination?.route ?: HealthTrackerScreen.Login    .name
+    )
 
+    //TODO: Add ViewModel
+
+    Scaffold (
+        topBar ={
+            HealthTrackerAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() })
+
+        }
+    )
+    { innerPadding ->
+
+        NavHost(
+            navController = navController,
+            startDestination = HealthTrackerScreen.Login.name,
+            modifier = Modifier.padding(innerPadding)
+        ){
+
+
+
+
+        }
+    }
 }
