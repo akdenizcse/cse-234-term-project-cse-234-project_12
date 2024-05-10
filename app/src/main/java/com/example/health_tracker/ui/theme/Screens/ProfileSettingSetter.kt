665b592a92757a5aa27ce50ef98600aba2a06cbb
CanDundarCSE <candundarcse@gmail.com>
2024-05-10 13:02:32 +0300

    Added colors to colors.xml

--- app/src/main/java/com/example/health_tracker/ui/theme/Screens/ProfileSettingSetter.kt
+++ app/src/main/java/com/example/health_tracker/ui/theme/Screens/ProfileSettingSetter.kt
@@ -1,2 +1,12 @@
 package com.example.health_tracker.ui.theme.Screens
 
+import android.graphics.Color
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.health_tracker.R
+
+@Preview
+@Composable
+fun ProfileSettings(){
+    val gradientColors = listOf(R.color.white,)
+}
