8c57988b61cf07eaecac10f2be7ba95142c87284
CanDundarCSE <candundarcse@gmail.com>
2024-05-10 14:08:50 +0300

    Created background

--- app/src/main/java/com/example/health_tracker/ui/theme/Screens/ProfileSettingSetter.kt
+++ app/src/main/java/com/example/health_tracker/ui/theme/Screens/ProfileSettingSetter.kt
@@ -1,12 +1,34 @@
 package com.example.health_tracker.ui.theme.Screens
 
 import android.graphics.Color
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.graphics.toColor
 import com.example.health_tracker.R
 
 @Preview
 @Composable
 fun ProfileSettings(){
-    val gradientColors = listOf(R.color.white,)
+    val gradientColors = listOf(
+        colorResource(id = R.color.dark_gray),
+        colorResource(id = R.color.light_gray),
+        colorResource(id = R.color.light)
+    )
+    //Background
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(colors = gradientColors)
+
+            )
+    ) {
+
+    }
 }
