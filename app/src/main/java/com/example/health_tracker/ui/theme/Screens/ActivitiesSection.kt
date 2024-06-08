eec6b37d0e1c6f8f1b2f06a7adc9fca9953fc28f
CanDundarCSE <candundarcse@gmail.com>
2024-06-08 16:41:42 +0300

    Created active activities

--- app/src/main/java/com/example/health_tracker/ui/theme/Screens/ActivitiesSection.kt
+++ app/src/main/java/com/example/health_tracker/ui/theme/Screens/ActivitiesSection.kt
@@ -1,9 +1,12 @@
 package com.example.health_tracker.ui.theme.Screens
 
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,6 +16,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -24,12 +29,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.health_tracker.R
 
 @Preview(
@@ -74,10 +84,128 @@ fun ActivitiesScreen(){
             colors = TextFieldDefaults.colors(
                 unfocusedIndicatorColor = Color.Transparent,
                 focusedIndicatorColor = Color.Transparent,
-                focusedContainerColor = Color.White,
+                focusedContainerColor = Color.Transparent,
                 unfocusedContainerColor = Color.White
             )
         )
+        Spacer(modifier = Modifier.height(50.dp))
 
+        Text(
+            text = "Active Activities",
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.padding(start = 100.dp)
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .border(
+                width = 2.dp,
+                color = Color.Black,
+                shape = RectangleShape
+            )){
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Card(
+                    modifier = Modifier
+                        .padding(start = 16.dp, top = 20.dp)
+                        .shadow(
+                            elevation = 20.dp,
+                            shape = RoundedCornerShape(12.dp),
+                            spotColor = Color.Black
+                        )
+                        .width(297.dp)
+                        .height(72.dp),
+                        colors = CardDefaults.cardColors(containerColor = Color(0xFFC8E3ED))
+                ) {
+                    Text(text = "Push Day",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 16.sp,
+                        ),
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 10.dp))
+                        Spacer(modifier = Modifier.height(3.dp))
+
+                    Text(text = "Incline Bench Press",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 10.sp,
+                        ),
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 5.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Card(
+                    modifier = Modifier
+                        .padding(start = 16.dp, top = 20.dp)
+                        .shadow(
+                            elevation = 20.dp,
+                            shape = RoundedCornerShape(12.dp),
+                            spotColor = Color.Black
+                        )
+                        .width(297.dp)
+                        .height(72.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFC8E3ED))
+                ) {
+                    Text(text = "Pull Day",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 16.sp,
+                        ),
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 10.dp))
+                    Spacer(modifier = Modifier.height(3.dp))
+
+                    Text(text = "Lat Pulldown",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 10.sp,
+                        ),
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 5.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Card(
+                    modifier = Modifier
+                        .padding(start = 16.dp, top = 20.dp)
+                        .shadow(
+                            elevation = 20.dp,
+                            shape = RoundedCornerShape(12.dp),
+                            spotColor = Color.Black
+                        )
+                        .width(297.dp)
+                        .height(72.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFC8E3ED))
+                ) {
+                    Text(text = "Leg Day",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 16.sp,
+                        ),
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 10.dp))
+                    Spacer(modifier = Modifier.height(3.dp))
+
+                    Text(text = "Leg Press",
+                        style = TextStyle(
+                            color = Color.Black,
+                            fontSize = 10.sp,
+                        ),
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 5.dp)
+                    )
+                }
+            }
+        }
     }
 }
