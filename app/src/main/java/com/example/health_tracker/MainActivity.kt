d6c2f241fd9251cff690828aca07b2bdabd6cc4f
İsmail Temüroğlu <ismailtemuroglu2819@gmail.com>
2024-06-13 19:00:28 +0300

    solve unused import

--- app/src/main/java/com/example/health_tracker/MainActivity.kt
+++ app/src/main/java/com/example/health_tracker/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.room.Room
 import com.example.health_tracker.data.ActivityViewModel
 import com.example.health_tracker.data.checkForPermission
 import com.example.health_tracker.data.isInternetAvailable
